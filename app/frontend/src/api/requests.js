31ba16b76f70c516f923628efc889a6575260e71
antoniowingert <tonho-bruno@outlook.com>
2023-05-30 17:58:21 -0300

    update env files

--- app/frontend/src/api/requests.js
+++ app/frontend/src/api/requests.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: `${process.env.API_URL || 'http://localhost:3001'}`,
+  baseURL: `${process.env.REACT_APP_API_URL || 'http://localhost:3001'}`,
 });
+
+console.log(process.env.REACT_APP_API_URL);
 
 export const requestData = async (endpoint) => {
   const { data } = await api.get(endpoint);
